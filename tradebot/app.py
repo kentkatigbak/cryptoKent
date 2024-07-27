17dcbaba3c8e07df367457b506e3560ab77d086b
kentkatigbak <102440753+kentkatigbak@users.noreply.github.com>
2024-07-27 21:33:18 +0800

    Update app.py

--- tradebot/app.py
+++ tradebot/app.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="KentTrades", layout="wide",page_icon="💰")
 # Titles and subtitles
 with st.sidebar:
     st.write("")
-    st.image("cryptoKent/tradebot/myLogo.png")
+    st.image("cryptoKent\tradebot\myLogo.png")
     st.title("")
     st.title("")
     st.title("")
