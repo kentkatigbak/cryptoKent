66e410e96aa001c4e7fb6164d61bf2b40031f85b
kentkatigbak <102440753+kentkatigbak@users.noreply.github.com>
2024-07-28 10:51:29 +0800

    Update app.py

--- tradebot/app.py
+++ tradebot/app.py
@@ -21,10 +21,10 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 st.markdown("""
         <style>
                 .block-container {
-                    padding-top: 1rem;
+                    padding-top: 0rem;
                     padding-bottom: 0rem;
-                    padding-left: 5rem;
-                    padding-right: 5rem;
+                    padding-left: 0rem;
+                    padding-right: 0rem;
                 }
         </style>
         """, unsafe_allow_html=True)
